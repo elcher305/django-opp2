860fcf7e64fb0e8e9cc58da78a19217697dbf99d
elect <elvira.chernova.05@gmail.com>
2025-10-08 23:06:57 +0700

    создание начальной миграций

--- design/settings.py
+++ design/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'design.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
--- design/urls.py
+++ design/urls.py
@@ -7,3 +7,8 @@ urlpatterns = [
     path('', include('catalog.urls')),
 
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

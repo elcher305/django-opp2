31ea3bca862f376ddaf648dc171426d72148d18f
elect <elvira.chernova.05@gmail.com>
2025-10-08 23:21:11 +0700

    добавление входа и выхода для япользователя

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
 
+
+class AdvUser(AbstractUser):
+    is_activated = models.BooleanField(default=True, db_index=True, verbose_name='Прошел активацию?')
+
+    class Meta(AbstractUser.Meta):
+        pass

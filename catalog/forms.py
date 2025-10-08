a7d7ffbd4d463377c4405b8df07e608d0404db70
elect <elvira.chernova.05@gmail.com>
2025-10-09 00:19:08 +0700

    Исправление формы регист раций

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth import password_validation
 from django.core.exceptions import ValidationError
 from .models import AdvUser
-
+from django.core.validators import RegexValidator
 
 class ChangeUserInfoForm(forms.ModelForm):
    email = forms.EmailField(required=True, label='Адрес электронной почты')
@@ -12,8 +12,12 @@ class ChangeUserInfoForm(forms.ModelForm):
        fields = ('username', 'email', 'fio')
 
 class RegisterUserForm(forms.ModelForm):
-    email = forms.EmailField(required=True, label='Адрес электронной почты')
-
+    email = forms.EmailField(required=True, label='Адрес электронной почты', validators=[
+        RegexValidator(
+            regex=r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+            message=' Необходим валидный формат email-адреса'
+        )
+    ])
     password1 = forms.CharField(
         label='Пароль',
         widget=forms.PasswordInput,help_text=password_validation.password_validators_help_text_html()
@@ -22,8 +26,18 @@ class RegisterUserForm(forms.ModelForm):
         label='Пароль (повторно)',
         widget=forms.PasswordInput,help_text='Повторите тот же самый пароль еще раз'
     )
-    fio = forms.CharField(label='ФИО', max_length=100)
-    username = forms.CharField(label='Логин', max_length=30)
+    fio = forms.CharField(label='ФИО', max_length=100, validators=[
+        RegexValidator(
+            regex=r'^[а-яА-ЯёЁ\s-]+$',
+            message='ФИО должно состоять только из кириллических букв, пробелов и дефисов.'
+        )
+    ])
+    username = forms.CharField(label='Логин', max_length=30, validators=[
+        RegexValidator(
+            regex=r'^[a-zA-Z-]+$',
+            message='Логин должен состоять только из латинских букв и дефисов.'
+        )
+    ])
     consent = forms.BooleanField(label='Согласие на обработку персональных данных')
 
     def clean_password1(self):
